fac53c90a7faf403bec22329156b82c4228fdf93
DanilByaliy <byaliy03@gmail.com>
2022-04-24 21:42:02 +0300

    Add node and list classes and also some methods
    The node class constructor returns an object with three fields: value,
    reference to the previous one to the next node. The list class
    co-constructor forms the head and tail fields of the list.
    The lengthFromTail method moves from the tail of the list until the
    reference to the previous element is empty, counting and returning the
    number of nodes. The lengthFromHead method works similarly, only from
    the head, I did it in order to compare the results of these methods and
    determine the correctness of other methods.
    The append method takes values, the tape type is checked, then the tail
    node becomes penultimate, and the tail field is replaced by a new node
    that has a link to the penultimate, if the list head is empty, the
    current node takes its role and the function ends the next element
    becomes a link to the tail.
    The insert method takes the value and position number in the list. We
    start to run from the head to the location of the node, if the number
    is greater than the number of nodes we see an error message. If the
    searched place is the end of the list, then we call the append method,
    otherwise we create a new node, assign it a link to neighboring nodes,
    and neighboring nodes - to it. If this is the beginning of the list,
    then change the header field with this element.

---
+++ index.js
@@ -0,0 +1,87 @@
+class Node {
+  constructor(value, prev) {
+    return { 
+      value: value, 
+      prev: prev, 
+      next: null };
+  }
+}
+
+class List {
+  constructor() {
+    this.head = null; //first elem
+    this.tail = null;
+  }
+
+  lengthFromTail() {
+    let counter = 0;
+    let current = this.tail;
+    while (current !== null) {
+      counter += 1;
+      current = current.prev;
+    }
+    return counter;
+  }
+
+  lengthFromHead() {
+    let counter = 0;
+    let current = this.head;
+    while (current !== null) {
+      counter += 1;
+      current = current.next;
+    }
+    return counter;
+  }
+
+  append(value) {
+    if (typeof value === 'string') {
+      const penultimate = this.tail;
+      this.tail = new Node(value, penultimate);
+
+      if (this.head === null) this.head = this.tail;
+      if (this.tail.prev === null) return;
+      this.tail.prev.next = this.tail;
+    } else console.log(new Error('Invalid value'));
+  }
+
+  insert(value, number) {
+    let counter = 0;
+    let current = this.head;
+
+    while (counter !== number) {
+      if (current === null || number < 0) {
+        console.log('Invalid number');
+        return;
+      }
+      counter += 1;
+      current = current.next;
+    };
+
+    if (current === null) {
+      this.append(value);
+      return;
+    }
+
+    const next = current;
+    const prev = current?.prev;
+
+    next.prev = current = new Node(value, prev);
+
+    if (number === 0) this.head = current;
+    else prev.next = current;
+
+    current.next = next;
+  }
+}
+
+// Usage
+
+let list = new List;
+
+list.append('0');
+list.append('1');
+list.append('2');
+list.insert('0.5', 1);
+list.insert('-1', 0);
+console.log('lenght1: ' + list.lengthFromHead());
+console.log('lenght2: ' + list.lengthFromTail());
