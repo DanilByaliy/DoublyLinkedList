b37ed7fc6f9bdcf00cbfd205793c0cd73db20282
DanilByaliy <byaliy03@gmail.com>
2022-05-02 01:07:26 +0300

    Add unit tests to all other methods
    Added tests to clone, reverse, findFirst, findLast, clear and extend
    methods

--- index.test.js
+++ index.test.js
@@ -176,8 +176,120 @@ describe('Clone metod', () => {
     const cloneList = list.clone();
 
     expect(list.clone).toBeDefined();
+    expect(cloneList.length()).toBe(2);
+    expect(cloneList.get(0)).toBe('0');
+    expect(cloneList.get(1)).toBe('1');
+    expect(() => cloneList.get(2)).toThrow('Invalid number');
+  })
+
+  test('should not have changes that occur with the initial list in the future', () => {
+    const cloneList = list.clone();
+    list.insert('2', 1);
+    list.delete(0);
+    list.append('3');
+
+    expect(list.get(0)).toBe('2');
+    expect(list.get(1)).toBe('1');
+    expect(list.get(2)).toBe('3');
+    expect(list.length()).toBe(3);
     expect(cloneList.get(0)).toBe('0');
     expect(cloneList.get(1)).toBe('1');
     expect(() => cloneList.get(2)).toThrow('Invalid number')
+    expect(cloneList.length()).toBe(2);
+  })
+})
+
+
+describe('Reverse metod', () => {
+  test('must be defined and delete all values passed by the argument', () => {
+    list.reverse();
+
+    expect(list.reverse).toBeDefined();
+    expect(list.length()).toBe(2);
+    expect(list.get(0)).toBe('1');
+    expect(list.get(1)).toBe('0');
+  })
+})
+
+
+describe('FindFirst metod', () => {
+  test('must be defined and return the index of the first node with the desired value', () => {
+    list.append('1');
+    const index = list.findFirst('1');
+
+    expect(list.findFirst).toBeDefined();
+    expect(index).toBe(1);
+  })
+
+  test('should return -1 if there is no node with the required value', () => {
+    const index = list.findFirst('2');
+
+    expect(index).toBe(-1);
+  })
+})
+
+
+describe('FindLast metod', () => {
+  test('must be defined and return the index of the last node with the desired value', () => {
+    list.append('1');
+    const index = list.findLast('1');
+
+    expect(list.findLast).toBeDefined();
+    expect(index).toBe(2);
+  })
+
+  test('should return -1 if there is no node with the required value', () => {
+    const index = list.findLast('2');
+
+    expect(index).toBe(-1);
+  })
+})
+
+
+describe('Clear metod', () => {
+  test('must be defined and clear the list', () => {
+    list.clear();
+
+    expect(list.clear).toBeDefined();
+    expect(list.length()).toBe(0);
+    // if there is no first node, then there are no nodes in the list at all:
+    expect(() => list.get(0)).toThrow('Invalid number'); 
+  })
+})
+
+
+describe('Extend metod', () => {
+  beforeEach(() => {
+    otherList = new List;
+    otherList.append('2');
+    otherList.append('3');
+  })
+
+  test('must be defined and add to the first list nodes with the values of the nodes of another', () => {
+    list.extend(otherList);
+
+    expect(list.extend).toBeDefined();
+    expect(list.length()).toBe(4);
+    expect(list.get(0)).toBe('0');
+    expect(list.get(1)).toBe('1');
+    expect(list.get(2)).toBe('2');
+    expect(list.get(3)).toBe('3');
+  })
+
+  test('after extension, the initial list should not change if another changes', () => {
+    list.extend(otherList);
+    otherList.insert('4', 1);
+    otherList.delete(0);
+    otherList.append('5');
+
+    expect(list.length()).toBe(4);
+    expect(list.get(0)).toBe('0');
+    expect(list.get(1)).toBe('1');
+    expect(list.get(2)).toBe('2');
+    expect(list.get(3)).toBe('3');
+    expect(otherList.length()).toBe(3);
+    expect(otherList.get(0)).toBe('4');
+    expect(otherList.get(1)).toBe('3');
+    expect(otherList.get(2)).toBe('5');
   })
 })
